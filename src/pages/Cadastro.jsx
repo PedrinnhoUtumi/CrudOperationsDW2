0939b90f0e2b9185445c553bc4e31255d96e55d7
Pedro Felipe Onofre Utumi <130059527+PedrinnhoUtumi@users.noreply.github.com>
2024-11-27 11:57:38 -0300

    login criado

---
+++ src/pages/Cadastro.jsx
@@ -0,0 +1,23 @@
+
+
+export function Cadastro() {
+    return (
+        <div className=" w-screen h-screen bg-orange-400 flex flex-col justify-center items-center">
+            <form action="get" className="bg-white w-80 h-1/2 flex flex-col justify-around items-center rounded-2xl">
+                <div className="p-4 m-1 mb-10 flex flex-col justify-around items-center">
+                    <label htmlFor="" className="mr-40">Usuário</label>
+                    <input type="text" className="m-2 p-1 w-52 bg-gray-100"/>
+                    
+                    <label htmlFor="" className="mr-40">Usuário</label>
+                    <input type="email" className="m-2 p-1 w-52 bg-gray-100"/>
+            
+                    <label htmlFor="" className="mr-40">Senha</label>
+                    <input type="password" className="m-2 p-1 w-52 bg-gray-100" />
+                    <button className="bg-red-500 mt-8 p-3 rounded-md text-white">CLIQUE AQUI</button>
+
+                    <p className="mt-10">Não tenho conta. <a href="Login.jsx" className="mt-12 text-orange-400">Cadastrar agora</a></p>
+                </div>
+            </form>
+        </div>
+    )
+}
